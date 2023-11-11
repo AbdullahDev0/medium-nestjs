158049c613683e38cc8cf758d04d4298ea6261a8
Abdullah Irfan <abdullahirfandev@gmail.com>
2023-11-11 09:36:53 +0500

    Update getOAuth2Client.ts

--- src/shared/utils/getOAuth2Client.ts
+++ src/shared/utils/getOAuth2Client.ts
@@ -1,8 +1,11 @@
 import * as fs from 'fs';
 import { CREDENTIALS_PATH, GMAIL_CREDS } from '../utils/constants';
 import { google } from 'googleapis';
+import { OAuth2Client } from 'google-auth-library/build/src';
 
-export default function getOAuthClient(token: object | null = null) {
+export default function getOAuthClient(
+  token: object | null = null,
+): OAuth2Client {
   const content = fs.readFileSync(CREDENTIALS_PATH + GMAIL_CREDS, 'utf-8');
   const credentials = JSON.parse(content);
   const { client_secret, client_id, redirect_uris } = credentials.web;
